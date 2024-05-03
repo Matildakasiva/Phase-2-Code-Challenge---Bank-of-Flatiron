e46f94f4e56912c90f026d9a04bdc8111b845780
Matilda <ogarimatilda@gmail.com>
2024-05-03 09:17:28 +0300

    changes

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Search({ transactions, onSearch }) {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleSearch = (event) => {
-    const searchTermValue = event.target.value
-    setSearchTerm(searchTermValue);
-    
-    const  filtered = transactions.filter((transaction) => {
-      return transaction.description.toLowercase().includes(searchTermValue.toLowerCase())
-    })
-    onSearch(filtered)
-  };
-
-
+function Search({ handleSearch }) {
+  
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder="Search your Recent Transactions"
-        value={searchTerm}
         onChange={handleSearch}
       />
       <i className="circular search link icon"></i>
